4fd9010a2ee4050858aaddeec68ab487c60365e0
alexhkz <alexwebcode@gmail.com>
2020-11-13 20:10:09 +0300

    add-components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav className='nav'>
+			<div>
+				<a href="#">Profile</a>
+			</div>
+			<div>
+				<a href="#">Messages</a>
+			</div>
+			<div>
+				<a href="#">News</a>
+			</div>
+			<div>
+				<a href="#">Music</a>
+			</div>
+			<div>
+				<a href="#">Settings</a>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src="https://theinpaint.com/images/example-1-2.jpg" alt="main" />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New posts
+				</div>
+				<div>
+					<div>
+						New posts
+						</div>
+					<div>
+						New posts
+						</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
