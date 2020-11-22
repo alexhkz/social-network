ad94ecb57e47e6e996304bbd7cf40530538f833e
alexhkz <alexwebcode@gmail.com>
2020-11-22 21:18:09 +0300

    add-post-props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
 	return (
 		<div className={classes.item}>
 			<img src="https://image.freepik.com/free-vector/man-avatar-profile-on-round-icon_24640-14044.jpg" alt="man-avatar"></img>
-				post1
+			{props.message}
 			<div>
 				<span>like</span>
 			</div>
