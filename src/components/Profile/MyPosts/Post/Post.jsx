c287ba854c7e9e8b2f2ce9dbd25176ae2121114c
alexhkz <alexwebcode@gmail.com>
2020-11-22 18:34:40 +0300

    add-posts

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Post.module.css';
+
+const Post = () => {
+	return (
+		<div className={classes.item}>
+			<img src="https://image.freepik.com/free-vector/man-avatar-profile-on-round-icon_24640-14044.jpg" alt="man-avatar"></img>
+				post1
+			<div>
+				<span>like</span>
+			</div>
+		</div>
+	)
+}
+
+export default Post;
