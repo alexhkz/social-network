dcef5d476d4340a03ad40775e85c5567a4d3a6b1
alexhkz <alexwebcode@gmail.com>
2020-11-24 14:28:43 +0300

    add-navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
 			<div className={`${classes.item} ${classes.active}`}>
-				<a href="/profile">Profile</a>
+				<NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
 			</div>
 			<div className={classes.item}>
-				<a href="/dialogs">Messages</a>
+				<NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
 			</div>
 			<div className={classes.item}>
-				<a href="/news">News</a>
+				<NavLink to="/news" activeClassName={classes.active}>News</NavLink>
 			</div>
 			<div className={classes.item}>
-				<a href="/music">Music</a>
+				<NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
 			</div>
 			<div className={classes.item}>
-				<a href="/settings">Settings</a>
+				<NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink>
 			</div>
 		</nav>
 	)
