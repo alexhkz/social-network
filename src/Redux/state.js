264b4e403c870720af5a53f2fc13b6bbba8e51bb
alexhkz <alexwebcode@gmail.com>
2020-11-26 18:25:17 +0300

    create-state.js

---
+++ src/Redux/state.js
@@ -0,0 +1,35 @@
+let state = {
+
+	profilePage: {
+		posts: [
+			{ id: 1, message: 'Hi!' },
+			{ id: 2, message: 'Sup man?' },
+			{ id: 3, message: 'Yo bro!' },
+			{ id: 4, message: 'Hey!' },
+			{ id: 5, message: 'Privet!' },
+			{ id: 6, message: 'How are you?' }
+		]
+	},
+	dialogsPage: {
+		messages: [
+			{ id: 1, message: 'Hi!' },
+			{ id: 2, message: 'Sup man?' },
+			{ id: 3, message: 'Yo bro!' },
+			{ id: 4, message: 'Hey!' },
+			{ id: 5, message: 'Privet!' },
+			{ id: 6, message: 'How are you?' }
+		],
+		
+		dialogs: [
+			
+			{ id: 1, name: 'Alex' },
+			{ id: 2, name: 'Andrew' },
+			{ id: 3, name: 'Mike' },
+			{ id: 4, name: 'Anna' },
+			{ id: 5, name: 'Victor' },
+			{ id: 6, name: 'Irene' }
+		]
+	}
+}
+
+export default state;
--- src/index.js
+++ src/index.js
@@ -3,31 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './Redux/state';
 
-let posts = [
-	{ id: 1, message: 'Hi, how are you?', likesCount: 12 },
-	{ id: 2, message: "It's my first post", likesCount: 11 }
-];
-
-let dialogs = [
-	{ id: 1, name: 'Alex' },
-	{ id: 2, name: 'Andrew' },
-	{ id: 3, name: 'Mike' },
-	{ id: 4, name: 'Anna' },
-	{ id: 5, name: 'Victor' },
-	{ id: 6, name: 'Irene' }
-];
-
-let messages = [
-	{ id: 1, message: 'Hi!' },
-	{ id: 2, message: 'Sup man?' },
-	{ id: 3, message: 'Yo bro!' },
-	{ id: 4, message: 'Hey!' },
-	{ id: 5, message: 'Privet!' },
-	{ id: 6, message: 'How are you?' }
-];
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
