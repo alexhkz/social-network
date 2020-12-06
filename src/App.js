ad6d1d326667c3b07deb1e0e93411b52b5a273e7
alexhkz <alexwebcode@gmail.com>
2020-12-06 16:28:02 +0300

    add-dispatch

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ const App = (props) => {
 			<Route exact path='/profile' 
 				render={ () => <Profile 
 					profilePage={props.state.profilePage} 
-					addPost={props.addPost}
-					updateNewPostText={props.updateNewPostText} /> } />
+					dispatch={props.dispatch} /> } />
 			<Route path='/dialogs' render={ () => 
 				<Dialogs state={props.state.dialogsPage} />  } />
 			<Route path='/news' render={ () => <News /> } />
