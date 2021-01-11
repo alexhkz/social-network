bbd435dd4c0c914fceaee62e27967c0c96909e12
alexhkz <alexwebcode@gmail.com>
2021-01-11 15:55:47 +0300

    add-axios.get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
-
 	if (props.users.length === 0) {
-		props.setUsers([
-			{
-				id: 1, photoUrl: 'https://i.pinimg.com/736x/9f/08/95/9f08957fdb87a044dd6b8b5ddea8b024.jpg',
-				followed: false, fullName: 'Dmitri', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' }
-			},
-			{
-				id: 2, photoUrl: 'https://i.pinimg.com/736x/9f/08/95/9f08957fdb87a044dd6b8b5ddea8b024.jpg',
-				followed: true, fullName: 'Alexander', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' }
-			},
-			{
-				id: 3, photoUrl: 'https://i.pinimg.com/736x/9f/08/95/9f08957fdb87a044dd6b8b5ddea8b024.jpg',
-				followed: false, fullName: 'Andrew', status: 'I am a another boss', location: { city: 'Kiev', country: 'Ukraine' }
-			}
-		]);
+		axios
+			.get('https://social-network.samuraijs.com/api/1.0/users')
+			.then(response => {
+				props.setUsers(response.data.items);
+			});
 	}
 	return <div>
 		{
 			props.users.map((u) => <div key={u.id}>
 				<span>
 					<div>
-						<img src={u.photoUrl} className={styles.userPhoto} alt='ava' />
+						<img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt='ava' />
 					</div>
 					<div>
 						{u.followed
@@ -34,12 +26,12 @@ let Users = (props) => {
 				</span>
 				<span>
 					<span>
-						<div>{u.fullName}</div>
+						<div>{u.name}</div>
 						<div>{u.status}</div>
 					</span>
 					<span>
-						<div>{u.location.country}</div>
-						<div>{u.location.city}</div>
+						<div>{'u.location.country'}</div>
+						<div>{'u.location.city'}</div>
 					</span>
 				</span>
 			</div>)
