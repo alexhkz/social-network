64d2dd5c2641b0dbbacb3bb25adcc95fdaa4405e
alexhkz <alexwebcode@gmail.com>
2021-05-21 17:31:12 +0300

    add-thunk2

--- src/api/api.js
+++ src/api/api.js
@@ -17,9 +17,17 @@ export const usersAPI = {
 					}); 
 	},
 	follow(userId) {
-		return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+		return instance.post(`follow/${userId}`)
 	},
 	unFollow(userId) {
-		return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+		return instance.delete(`follow/${userId}`)
+	},
+	getProfile(userId) {
+		return instance.get(`profile/` + userId);
+	}
+}
+export const authAPI = {
+	me() {
+		return instance.get(`auth/me`)
 	}
 }
