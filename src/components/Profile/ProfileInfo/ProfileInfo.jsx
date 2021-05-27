217ab1acfeff68ffdf7ad53677389ffd241160d0
alexhkz <alexwebcode@gmail.com>
2021-05-27 16:32:30 +0300

    add status setState local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from '../../Common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 
@@ -10,12 +11,12 @@ const ProfileInfo = (props) => {
 
 	return (
 		<div>
-			<div>
+			{/* <div>
 				<img src="https://theinpaint.com/images/example-1-2.jpg" alt="main" />
-			</div>
+			</div> */}
 			<div className={classes.descriptionBlock}>
 				<img src={props.profile.photos.large} alt='ava-large' />
-				ava + description
+				<ProfileStatus status={'Hello my friends'} />
 			</div>
 		</div>
 	)
