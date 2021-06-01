ba024d1e704d367bb7daaa661fca1ceca239ae67
alexhkz <alexwebcode@gmail.com>
2021-06-01 15:49:49 +0300

    local state

--- src/components/Profile/ProfileInfo/ProfileContainer.jsx
+++ src/components/Profile/ProfileInfo/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Profile from './Profile';
 import { connect } from 'react-redux';
-import { getUserProfile } from '../../../Redux/profile-reducer';
+import { getUserProfile, getStatus, updateStatus } from '../../../Redux/profile-reducer';
 import { withRouter } from 'react-router-dom';
 import { withAuthRedirect } from '../../../Hoc/withAuthRedirect';
 import { compose } from 'redux';
@@ -14,20 +14,22 @@ class ProfileContainer extends React.Component {
 			userId = 2;
 		}
 		this.props.getUserProfile(userId);
+		this.props.getStatus(userId);
 	}
 
 	render() {
 		return (
-			<Profile {...this.props} profile={this.props.profile} />
+			<Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus} />
 		)
 	}
 }
 
 let mapStateToProps = (state) => ({
-	profile: state.profilePage.profile
+	profile: state.profilePage.profile,
+	status: state.profilePage.status
 });
 
 export default compose(
-	connect(mapStateToProps, { getUserProfile }),
+	connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
 	withRouter
 )(ProfileContainer);
