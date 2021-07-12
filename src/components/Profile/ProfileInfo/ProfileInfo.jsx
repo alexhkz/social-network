2490bbb073f69c1412b030ca8bc4bbaaaff494d7
alexhkz <alexwebcode@gmail.com>
2021-07-12 14:57:37 +0300

    useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from '../../Common/Preloader/Preloader';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
 
@@ -16,7 +17,7 @@ const ProfileInfo = (props) => {
 			</div> */}
 			<div className={classes.descriptionBlock}>
 				<img src={props.profile.photos.large} alt='ava-large' />
-				<ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+				<ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
 			</div>
 		</div>
 	)
