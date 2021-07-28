50460c9776dff7fec590a8c989dcd27ad084f596
alexhkz <alexwebcode@gmail.com>
2021-07-28 17:18:42 +0300

    add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileInfo/ProfileContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import { BrowserRouter, Route, withRouter } from 'react-router-dom';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
@@ -15,6 +13,11 @@ import { compose } from 'redux';
 import { initializeApp } from './Redux/app-reducer';
 import Preloader from './components/Common/Preloader/Preloader';
 import store from './Redux/redux-store';
+import { withSuspense } from './Hoc/withSuspense';
+// import ProfileContainer from './components/Profile/ProfileInfo/ProfileContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileInfo/ProfileContainer'));
 
 class App extends Component {
 	componentDidMount() {
@@ -32,17 +35,23 @@ class App extends Component {
 				<Navbar />
 				<div className='app-wrapper-content'>
 					<Route exact path='/profile/:userId?' 
-						render={ () => <ProfileContainer /> } />
+						render={ withSuspense(ProfileContainer)} />
+
 					<Route path='/dialogs' 
-						render={ () => <DialogsContainer /> } />
+						render={ withSuspense(DialogsContainer)} />
+
 					<Route path='/login' 
 						render={ () => <LoginPage /> } />
+
 					<Route path='/news' 
 						render={ () => <News /> } />
+
 					<Route path='/music' 
 						render={ () => <Music /> } />
+
 					<Route path='/settings' 
 						render={ () => <Settings /> } />
+						
 					<Route path='/users' 
 						render={ () => <UsersContainer /> } />
 				</div>
--- src/components/Common/Preloader/Preloader.js
+++ src/components/Common/Preloader/Preloader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import preloader from '../../../assets/images/preloader.svg';
 
 let Preloader = (props) => {
-	return <div style={{ backgroundColor: 'white' }}>
+	return <div style={{ backgroundColor: 'white', width: 50 }}>
 	<img src={preloader} alt='loading' />
 </div>
 }
